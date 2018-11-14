a02b8f638da8e8c858e4a518d40e9136725f74ca
pyaenaing <43054337+pyaenaing@users.noreply.github.com>
2018-11-13 20:15:16 -0800

    what joel gave us but in the react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,18 +7,29 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="header">O-mail</h1>
+          <div className="sidenav">
+            <a href="Inbox">Send</a>
+            <a href="Inbox">Inbox</a>
+            <a href="Sent">Sent</a>
+            <a href="Drafts">Drafts</a>
+            <a href="Trash">Trash</a>
+
+
+          </div>
+          <div className="main">
+            <div className="search-container">
+              <input type="search" placeholder="Search" />
+              <button type="submit"><i className="fa fa-search"></i></button>
+            </div>
+            <div className="rows">
+              <ul>
+                <li>Jsamcam - This is an email subject line</li>
+                <li>GitHub  - Welcome to Github</li>
+              </ul>
+            </div>
+
+          </div>
         </header>
       </div>
     );

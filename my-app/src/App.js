f1b7cd7068c3dc37b94dc5a5c22d5c7fce7b7aa1
Nick Besse <nbesse@mail.sfsu.edu>
2018-11-13 21:01:00 -0800

    React buttons to UI and general setup

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+
+
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }));
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="header">O-mail</h1>
           <div className="sidenav">
+
+                <button onClick={this.handleClick}>
+              {this.state.isToggleOn ? 'Send': 'Message Sent!'}
+              </button>
+
+            <button onClick={this.handleClick}>
+            {this.state.isToggleOn ? 'Inbox': 'Display Inbox'}</button>
+
+            {/* <button onClick={dosomething}>
+            Sent</button>
+
+            <button onClick={dosomething}>
+            Drafts</button>
+
+            <button onClick={dosomethingelse}>
+            Trash</button>  */}
+
             <a href="Inbox">Send</a>
             <a href="Inbox">Inbox</a>
             <a href="Sent">Sent</a>
@@ -35,5 +67,7 @@ class App extends Component {
     );
   }
 }
+
+
 
 export default App;

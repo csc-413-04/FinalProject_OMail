f628154027af9f1ef9274c517cdc02208c349253
pnaing@mail.sfsu.edu <pnaing@mail.sfsu.edu>
2018-11-14 17:39:39 -0800

    routed one page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,95 +1,25 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
+import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
-
+import mail from "./mail";
+import Login from "./Login";
 
 
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {isToggleOn: true};
 
-    // This binding is necessary to make `this` work in the callback
-    this.handleClick = this.handleClick.bind(this);
-  }
-  handleClick() {
-    this.setState(state => ({
-      isToggleOn: !state.isToggleOn
-    }));
-  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="red ui header">O-mail</h1>
-          <div class="sidenav">
-            <div className="fluid ui vertical buttons">
-              <button className="ui button" onClick={this.handleClick}>
-              {this.state.isToggleOn ? 'Send': 'Message Sent!'}
-              </button>
-
-              <button className="fluid ui button" onClick={this.handleClick}>
-              {this.state.isToggleOn ? 'Inbox': 'Display Inbox'}</button>
-
-            {/* <button onClick={dosomething}>
-            Sent</button>
-
-            <button onClick={dosomething}>
-            Drafts</button>
-
-            <button onClick={dosomethingelse}>
-            Trash</button>  */}
-
-              <button href="Sent" className="fluid ui button">Sent</button>
-              <button href="Drafts" className="fluid ui button">Drafts</button>
-              <button href="Trash" className="fluid ui button">Trash</button>
-            </div>
+      <div classname="App">
+        <Router>
+          <div>
+          <Switch>
+            <Route path="/logged" component={mail} />
+            <Route path="/" component={Login} />
+          </Switch>
           </div>
-          <div className="main">
-            <div className="search-container">
-              <div className="fluid ui action input">
-                  <input type="text" placeholder="Search..."></input>
-                  <button className="ui button">Search</button>
-              </div>
-            </div>
-            <div className="rows">
-                <table className="ui fixed table">
-                    <thead>
-                    <tr>
-                        <th>From</th>
-                        <th>Subject</th>
-                        <th>Received</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>John</td>
-                        <td>Approved</td>
-                        <td>John is an interesting boy but sometimes you don't really have enough room to describe
-                            everything you'd like
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Jamie</td>
-                        <td>Approved</td>
-                        <td>Jamie is a kind girl but sometimes you don't really have enough room to describe everything
-                            you'd like
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Jill</td>
-                        <td>Denied</td>
-                        <td>Jill is an alright girl but sometimes you don't really have enough room to describe
-                            everything you'd like
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>
-            </div>
+        </Router>
 
-          </div>
-        </header>
       </div>
     );
   }
---
+++ my-app/src/mail.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import './App.css'
+
+
+class  mail extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {isToggleOn: true};
+    
+        // This binding is necessary to make `this` work in the callback
+        this.handleClick = this.handleClick.bind(this);
+      }
+      handleClick() {
+        this.setState(state => ({
+          isToggleOn: !state.isToggleOn
+        }));
+      }
+      
+    render() {
+        return (
+            <div className="App">
+            <header className="App-header">
+              <h1 className="red ui header">O-mail</h1>
+              <div class="sidenav">
+                <div className="fluid ui vertical buttons">
+                  <button className="ui button" onClick={this.handleClick}>
+                  {this.state.isToggleOn ? 'Send': 'Message Sent!'}
+                  </button>
+    
+                  <button className="fluid ui button" onClick={this.handleClick}>
+                  {this.state.isToggleOn ? 'Inbox': 'Display Inbox'}</button>
+    
+    
+                  <button href="Sent" className="fluid ui button">Sent</button>
+                  <button href="Drafts" className="fluid ui button">Drafts</button>
+                  <button href="Trash" className="fluid ui button">Trash</button>
+                </div>
+              </div>
+              <div className="main">
+                <div className="search-container">
+                  <div className="fluid ui action input">
+                      <input type="text" placeholder="Search..."></input>
+                      <button className="ui button">Search</button>
+                  </div>
+                </div>
+                <div className="rows">
+                    <table className="ui fixed table">
+                        <thead>
+                        <tr>
+                            <th>From</th>
+                            <th>Subject</th>
+                            <th>Received</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>John</td>
+                            <td>Approved</td>
+                            <td>John is an interesting boy but sometimes you don't really have enough room to describe
+                                everything you'd like
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Jamie</td>
+                            <td>Approved</td>
+                            <td>Jamie is a kind girl but sometimes you don't really have enough room to describe everything
+                                you'd like
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Jill</td>
+                            <td>Denied</td>
+                            <td>Jill is an alright girl but sometimes you don't really have enough room to describe
+                                everything you'd like
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+    
+              </div>
+            </header>
+          </div>
+        );
+    }
+}
+
+export default mail;

6cb8b47d90d9c4c5359700fc5189df303e0d0fd8
Joel Samaniego Campos <jsamani1@mail.sfsu.edu>
2018-11-14 21:44:35 -0800

    clean up old css, add icons and make the rows more compact

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -22,14 +22,14 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="red ui header">O-mail</h1>
+          <h1 className="red ui header"><i className="envelope open outline icon"></i>O-mail</h1>
           <div class="sidenav">
-            <div className="fluid ui vertical buttons">
-              <button className="ui button" onClick={this.handleClick}>
+            <div className="fluid ui large vertical buttons">
+              <button className="ui primary button" onClick={this.handleClick}><i className="envelope icon"></i>
               {this.state.isToggleOn ? 'Send': 'Message Sent!'}
               </button>
 
-              <button className="fluid ui button" onClick={this.handleClick}>
+              <button className="fluid ui button" onClick={this.handleClick}><i className="inbox icon"></i>
               {this.state.isToggleOn ? 'Inbox': 'Display Inbox'}</button>
 
             {/* <button onClick={dosomething}>
@@ -41,20 +41,20 @@ class App extends Component {
             <button onClick={dosomethingelse}>
             Trash</button>  */}
 
-              <button href="Sent" className="fluid ui button">Sent</button>
-              <button href="Drafts" className="fluid ui button">Drafts</button>
-              <button href="Trash" className="fluid ui button">Trash</button>
+              <button href="Sent" className="fluid ui button"><i className="paper plane icon"></i>Sent</button>
+              <button href="Drafts" className="fluid ui button"><i className="file icon"></i>Drafts</button>
+                <button href="Trash" className="fluid ui button"><i class="trash icon"></i>Trash</button>
             </div>
           </div>
           <div className="main">
             <div className="search-container">
               <div className="fluid ui action input">
                   <input type="text" placeholder="Search..."></input>
-                  <button className="ui button">Search</button>
+                  <button className="ui button"><i className="search icon"></i>Search</button>
               </div>
             </div>
             <div className="rows">
-                <table className="ui fixed table">
+                <table className="ui striped compact selectable celled table">
                     <thead>
                     <tr>
                         <th>From</th>
@@ -64,24 +64,30 @@ class App extends Component {
                     </thead>
                     <tbody>
                     <tr>
-                        <td>John</td>
+                        <td>Osbaldo</td>
                         <td>Approved</td>
                         <td>John is an interesting boy but sometimes you don't really have enough room to describe
                             everything you'd like
                         </td>
                     </tr>
                     <tr>
-                        <td>Jamie</td>
+                        <td>Sawyer</td>
                         <td>Approved</td>
                         <td>Jamie is a kind girl but sometimes you don't really have enough room to describe everything
                             you'd like
                         </td>
                     </tr>
                     <tr>
-                        <td>Jill</td>
+                        <td>Nick</td>
                         <td>Denied</td>
                         <td>Jill is an alright girl but sometimes you don't really have enough room to describe
                             everything you'd like
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>JJ</td>
+                        <td>Sad</td>
+                        <td>I can't believe Stan Lee died :(
                         </td>
                     </tr>
                     </tbody>

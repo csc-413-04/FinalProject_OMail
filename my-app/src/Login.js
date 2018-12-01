c9ce44d2016df979d6f93f49587a2089da527ed2
pnaing@mail.sfsu.edu <pnaing@mail.sfsu.edu>
2018-12-01 14:15:53 -0800

    login can connect to spark now

--- my-app/src/Login.js
+++ my-app/src/Login.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import axios from 'axios';
 import "./Login.css";
+
 
 export default class Login extends Component {
   constructor(props) {
@@ -16,6 +18,31 @@ export default class Login extends Component {
     return this.state.email.length > 0 && this.state.password.length > 0;
   }
 
+  sendSomeData = (e) => {
+    axios({
+        method : 'POST',
+        url: '/create',
+        data: {
+            user: this.state.email,
+            password: this.state.password,
+        }
+    })
+    .then((res)=> {
+        console.log(res)
+    }).catch((e) =>{
+        console.log(e);
+    });
+    this.setState({
+        email: '',
+        password: ''
+    })
+}
+
+  displayLog = (e) =>{
+    console.log(this.state.email);
+    console.log(this.state.password);
+  }
+
   handleChange = event => {
     this.setState({
       [event.target.id]: event.target.value
@@ -26,16 +53,28 @@ export default class Login extends Component {
     event.preventDefault();
   }
 
+  updateEmail(e){
+        this.setState({
+            email: e.target.value,
+        })
+    }
+    
+  updatePass(e){
+      this.setState({
+          password: e.target.value,
+      })
+  }
+
   render() {
     return (
       <div className="Login">
-        <h1 className="red ui header"><i className="envelope open outline icon"></i>O-mail</h1>
+        <h1 class="red ui header">O-mail</h1>
         <form onSubmit={this.handleSubmit}>
           <FormGroup controlId="email" bsSize="large">
             <ControlLabel>Email:</ControlLabel>
             <FormControl
               autoFocus
-              type="email"
+              type="text"
               value={this.state.email}
               onChange={this.handleChange}
             />
@@ -53,12 +92,10 @@ export default class Login extends Component {
             bsSize="large"
             disabled={!this.validateForm()}
             type="submit"
+            onClick={this.sendSomeData}
           >
             Login
           </Button>
-        </form>
-        <form action="/CreateUser" class="inline">
-          <button class="float-center submit-button" type="submit"> Create User</button>
         </form>
       </div>
     );
--- omail/src/Main.java
+++ omail/src/Main.java
@@ -1,16 +1,39 @@
-import com.google.gson.Gson;
+import spark.Request;
+import spark.Response;
+
 import static spark.Spark.*;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Set;
+
 
 public class Main {
 
-    public static void main(String[] args){
-        port(1234);
-        get("/newuser", (req, res)->{
-
-            return "hi";
-        });
-
+    public static String processRoute(Request req, Response res) {
+        Set<String> params = req.queryParams();
+        for (String param : params) {
+            // possible for query param to be an array
+            System.out.println(param + " : " + req.queryParamsValues(param)[0]);
+        }
+        // do stuff with a mapped version http://javadoc.io/doc/com.sparkjava/spark-core/2.8.0
+        // http://sparkjava.com/documentation#query-maps
+        // print the id query value
+        System.out.println(req.queryMap().get("id").value());
+        return "done!";
     }
 
+    public static void main(String[] args) {
+
+        port(1234);
+
+        post("/create", (req, res) -> {
+            String body = req.body();
+            System.out.println(body);
+            return "OK";
+        });
+
+
+    }
 
 }

333ccc2124c6773a165647a32d5317fe0f9a62c8
SawyerNixon <sawyer.nixon@gmail.com>
2018-12-03 14:42:11 -0800

    Completed working version of delete and move
    I used a bool in mail to keep track of trash status

--- omail/src/Database.java
+++ omail/src/Database.java
@@ -5,115 +5,131 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
+
 import javax.servlet.http.Cookie;
+
 import org.eclipse.jetty.server.Authentication;
+
 import java.util.*;
 import javax.print.Doc;
 import java.util.*;
 import java.util.ArrayList;
 
 public class Database {
-    private  static Database instance = null;
+  private static Database instance = null;
+  private MongoClient mongoClient;
+  private MongoDatabase db;
+  private MongoCollection<Document> myCollectionUsers;
+  private MongoCollection<Document> myCollectionMail;
+  private Database() {
+    mongoClient = new MongoClient("localhost", 27017);
+    db = mongoClient.getDatabase("REST2");
+    myCollectionUsers = db.getCollection("users ");
+    myCollectionMail = db.getCollection("mail ");
+  }
 
-    private  Database() {
-        mongoClient = new MongoClient("localhost", 27017);
-        db = mongoClient.getDatabase("REST2");
-        myCollectionUsers = db.getCollection("users ");
-        myCollectionMail = db.getCollection("mail ");
+  public static Database getInstance() {
+    if (instance == null) {
+      instance = new Database();
     }
+    return instance;
+  }
 
-    private MongoClient mongoClient;
-    private MongoDatabase db;
-    private MongoCollection<Document> myCollectionUsers;
-    private MongoCollection<Document> myCollectionMail;
+  public boolean loginCheck(String user, String pass) {
 
-    public static Database getInstance() {
-        if (instance == null) {
-            instance = new Database();
-        }
-        return instance;
+    try {
+      Document search = myCollectionUsers.find(eq("User", user)).first();
+      return (search.getString("User").equals(user) &&
+        search.getString("Password").equals(pass));
+    } catch (Exception e) {
+      return false;
     }
+  }
 
-    public boolean loginCheck(String user, String pass){
-
-        try {
-            Document search = myCollectionUsers.find(eq("User", user)).first();
-            return (search.getString("User").equals(user)&&
-                    search.getString("Password").equals(pass));
-        }catch (Exception e)
-        {
-            return false;
-        }
-    }
-
-    public void storeMail(Mail mail) {
-        //this method stores the mail in database
-        //return true if successful, false otherwise.
+  public void storeMail(Mail mail) {
+    //this method stores the mail in database
+    //return true if successful, false otherwise.
 //        Document doc = new Document("Sender", mail.getSender())
 //                .append("Recipient", mail.getRecipient()).append("Subject", mail.getSubject()).append("MailBody", mail.getMailBody()).append("Date", mail.getTimeDate()).append("IsUnread", mail.getIsUnread()).append("MailID",mail.getMailID());
-        Document doc = new Document("Sender", mail.getSender())
-                .append("Recipient", mail.getRecipient()).append("MailBody", mail.getMailBody());
-        myCollectionMail.insertOne(doc);
-        //Also copy the contents of the mail into the sending user so user has copy of mail
+    Document doc = new Document("Sender", mail.getSender())
+      .append("Recipient", mail.getRecipient()).append("MailBody", mail.getMailBody());
+    myCollectionMail.insertOne(doc);
+    //Also copy the contents of the mail into the sending user so user has copy of mail
+  }
+
+  public String[] showMail(String user, String mailType) {
+    //This method returns the list of mails that is specified in mailType
+    //if mailType == inbox return inbox mail list, if sent return sent mail list, if trash return trashed mail list
+    String[] x = new String[10000];
+    x[0] = "[]";
+    try {
+      Document search = myCollectionUsers.find(eq("User", user)).first();
+      Document t = (Document) search.get(mailType);
+      x[0] = t.values().toString();
+    } catch (Exception e) {
+      System.err.println("Error");
+    }
+    return x;
+  }
+
+  public ArrayList<String> showM(String user, String mailType) {
+    ArrayList<String> list = new ArrayList<String>();
+    MongoCursor<Document> cursor = myCollectionMail.find(eq(mailType, user)).iterator();
+    try {
+      while (cursor.hasNext()) {
+        list.add(cursor.next().toJson());
+      }
+    } finally {
+      cursor.close();
+    }
+    return list;
+  }
+
+  public Boolean moveMail(Mail mail, String destination) {
+    //this method moves the mail from current list to destination list.
+    //return true if successful, false otherwise.\
+    //TODO: Find better matching strategy
+    try {
+      Document search = myCollectionMail.find(eq("MailBody", mail.getMailBody())).first();
+    } catch (Exception e) {
+      return false;
+    }
+    if (destination.equals("Trash")) {
+      //search for where the mail is located by the mailID
+      //copy the contents of the mail
+      //delete the mail
+      //insert the copy into the trash
+      mail.moveToTrash();
+    }
+    return false;
+  }
+
+  public Boolean deleteMail(Mail mail) {
+    //this method permanently deletes the mail from database
+    //return true if successful, false otherwise.
+    //Mails can only be deleted if they are in trash
+    //TODO: Find better matching strategy
+    Document search;
+    try {
+      search = myCollectionMail.find(eq("MailBody", mail.getMailBody())).first();
+    } catch (Exception e) {
+      return false;
+    }
+    if(mail.isTrash()){
+      myCollectionMail.deleteOne(search);
     }
 
-    public String[] showMail(String user, String mailType) {
-        //This method returns the list of mails that is specified in mailType
-        //if mailType == inbox return inbox mail list, if sent return sent mail list, if trash return trashed mail list
-        String[] x = new String[10000];
-        x[0] = "[]";
-        try {
-            Document search = myCollectionUsers.find(eq("User", user)).first();
-            Document t = (Document) search.get(mailType);
-            x[0] = t.values().toString();
-        } catch (Exception e) {
-            System.err.println("Error");
-        }
-        return x;
-    }
+    return false;
+  }
 
-    public ArrayList<String> showM(String user, String mailType) {
-        ArrayList<String> list=new ArrayList<String>();
-        MongoCursor<Document> cursor = myCollectionMail.find(eq(mailType, user)).iterator();
-        try {
-            while (cursor.hasNext()) {
-                list.add(cursor.next().toJson());
-            }
-        } finally {
-            cursor.close();
-        }
-        return list;
+  public boolean createUser(String username, String password) {
+    Document search = myCollectionUsers.find(eq("User", username)).first();
+    if (search == null) {
+      Document doc = new Document("User", username)
+        .append("Password", password).append("Inbox", new Document()).append("Sent", new Document()).append("Trash", new Document());
+      myCollectionUsers.insertOne(doc);
+      return true;
     }
-
-    public Boolean moveMail(Mail mail, String destination) {
-        //this method moves the mail from current list to destination list.
-        //return true if successful, false otherwise.
-        if (destination.equals("Trash")) {
-            //search for where the mail is located by the mailID
-            //copy the contents of the mail
-            //delete the mail
-            //insert the copy into the trash
-        }
-        return false;
-    }
-
-    public Boolean deleteMail(Mail mail) {
-        //this method permanently deletes the mail from database
-        //return true if successful, false otherwise.
-        //Mails can only be deleted if they are in trash
-        return false;
-    }
-
-    public boolean createUser(String username, String password)
-    {
-        Document search = myCollectionUsers.find(eq("User", username)).first();
-        if(search == null)
-        {
-            Document doc = new Document("User", username)
-                    .append("Password", password).append("Inbox", new Document()).append("Sent", new Document()).append("Trash", new Document());
-            myCollectionUsers.insertOne(doc);
-            return true;
-        }
-        return false;
-    }
+    return false;
+  }
 }
--- omail/src/Mail.java
+++ omail/src/Mail.java
@@ -1,30 +1,48 @@
-
-
 public class Mail {
 
-    private String sender;
-    private String recipient;
-//    private String subject;
-    private String mailBody;
-//    private String mailID;
-    private String timeDate;
-//    private boolean isUnread = true;
+  private String sender;
+  private String recipient;
+  //    private String subject;
+  private String mailBody;
+  //    private String mailID;
+  private String timeDate;
+  //    private boolean isUnread = true;
+  private boolean trash = false;
 
-    public Mail(String from, String to, String mail){
-        sender = from;
-        recipient = to;
-        mailBody = mail;
-    }
+  public Mail(String from, String to, String mail) {
+    sender = from;
+    recipient = to;
+    mailBody = mail;
+  }
 
-    public String getSender() {return sender;}
-    public String getRecipient() {return recipient;}
-//    public String getSubject() {return subject;}
-    public String getMailBody() {return mailBody;}
-//    public String getMailID() {return mailID;}
-    public String getTimeDate() {return timeDate;}
+  public String getSender() {
+    return sender;
+  }
+
+  public String getRecipient() {
+    return recipient;
+  }
+
+  //    public String getSubject() {return subject;}
+  public String getMailBody() {
+    return mailBody;
+  }
+
+  //    public String getMailID() {return mailID;}
+  public String getTimeDate() {
+    return timeDate;
+  }
 //    public boolean getIsUnread() {return isUnread;}
 
-//    public void setMailID(String id){this.mailID = id;}
+  //    public void setMailID(String id){this.mailID = id;}
 //    public void mailRead(){this.isUnread = false;}
+  public boolean isTrash() {
+    return trash;
+  }
+
+  public void moveToTrash() {
+    trash = true;
+  }
+
 
 }

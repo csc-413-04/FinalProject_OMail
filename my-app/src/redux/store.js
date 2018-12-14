51cd104a0a92b5b614e940a26cc43ef7d3071251
Nick Besse <nbesse@mail.sfsu.edu>
2018-12-13 19:26:02 -0800

    Does this commit work, I broke my git

--- my-app/src/redux/store.js
+++ my-app/src/redux/store.js
@@ -4,4 +4,5 @@ import rootReducer from './rootReducer';
 //creates the redux store from all of the reducers that are referenced in the rootReducer
 const store = createStore(rootReducer);
 
+// let's see if this works
 export default store;

02aae4351436bda08f8b242ac74884f5c714d9f4
pnaing@mail.sfsu.edu <pnaing@mail.sfsu.edu>
2018-12-14 19:34:12 -0800

    auto load and other button should be working in mail.js

--- my-app/src/redux/rootReducer.js
+++ my-app/src/redux/rootReducer.js
@@ -1,12 +1,13 @@
 import {combineReducers} from 'redux';
-import MailReducer from './mailReducer';
+import mailReducer from './mailReducer';
+import selectedMail from './selectedMailReducer';
 import mailEditReducer from './mailEditReducer';
-import userReducer from './userReducer'
+import mailPreviewList from '../mailPreviewList';
 
 //combines the various reducers that will be used in the project into one large JS Object
 const rootReducers = combineReducers({
-    EmailList: MailReducer,
-    login: userReducer
+    EmailList: mailReducer,
+    mailEditReducer
 });
 
 export default rootReducers;

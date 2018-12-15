169568a627c69a4ddff0eca38b95a41179b30e55
Nick Besse <nbesse@mail.sfsu.edu>
2018-12-14 18:35:50 -0800

    what I meant to do to begin with, uncapitalizing mailEditReducer

--- my-app/src/redux/rootReducer.js
+++ my-app/src/redux/rootReducer.js
@@ -1,11 +1,11 @@
 import {combineReducers} from 'redux';
-import MailReducer from './mailReducer';
+import mailReducer from './mailReducer';
 import mailEditReducer from './mailEditReducer';
 import userReducer from './userReducer'
 
 //combines the various reducers that will be used in the project into one large JS Object
 const rootReducers = combineReducers({
-    EmailList: MailReducer,
+    EmailList: mailReducer,
     login: userReducer
 });
 

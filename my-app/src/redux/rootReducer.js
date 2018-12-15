ee26235f50d0345fc914b93188e010c0dba0fb36
John Joshua L Gutierrez <jgutie11@mail.sfsu.edu>
2018-12-15 13:12:11 -0800

    updating stuff

--- my-app/src/redux/rootReducer.js
+++ my-app/src/redux/rootReducer.js
@@ -1,8 +1,8 @@
 import {combineReducers} from 'redux';
 import MailReducer from './mailReducer';
 import selectedMail from './selectedMailReducer';
-import {res} from '../mail'
 import mailPreviewList from '../mailPreviewList';
+import Mail, {res} from '../mail';
 
 //combines the various reducers that will be used in the project into one large JS Object
 const rootReducers = combineReducers({
